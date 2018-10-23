0c89d10f2fd38a0c42f1fb83ac8f5d3fa6113e3a
Konrad Bösche <konrad.boesche@tomtom.com>
2018-10-23 12:23:46 +0200

    * Fix imports in ParkingSpotsRestController * Update .gitignore

--- src/main/java/com/tomtom/parkathon/rest/ParkingSpotsRestController.java
+++ src/main/java/com/tomtom/parkathon/rest/ParkingSpotsRestController.java
@@ -1,21 +1,20 @@
 package com.tomtom.parkathon.rest;
 
-import java.util.List;
-
-import org.assertj.core.util.Lists;
+import com.tomtom.parkathon.domain.ParkingSpot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tomtom.parkathon.domain.ParkingSpot;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class ParkingSpotsRestController {
 
-	@GetMapping("/parking-spots")
-	public List<ParkingSpot> getAvailableParkingSpots() {
-		return Lists.newArrayList(
-				new ParkingSpot(52000000L, 13000000L),
-				new ParkingSpot(52000000L, 13000000L),
-				new ParkingSpot(52000000L, 13000000L));
-	}
+    @GetMapping("/parking-spots")
+    public List<ParkingSpot> getAvailableParkingSpots() {
+        return Arrays.asList(
+                new ParkingSpot(52000000L, 13000000L),
+                new ParkingSpot(52000000L, 13000000L),
+                new ParkingSpot(52000000L, 13000000L));
+    }
 }
